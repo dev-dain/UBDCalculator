e1f7142717b69fea6fc1f4e3de4ab278c6a288a3
Jaimie <dev.jaimie@gmail.com>
2019-07-22 22:42:38 +0900

    Initial Commit

---
+++ UBDCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UBDCalculator
+//
+//  Created by Dain Kim on 22/07/2019.
+//  Copyright © 2019 Dain Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
