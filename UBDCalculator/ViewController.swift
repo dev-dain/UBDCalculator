ff82771912e5997bd7399b50897b2867073ab8bc
dev-dain <dev.dain.k@gmail.com>
2019-07-23 22:18:43 +0900

    calculator part

--- UBDCalculator/ViewController.swift
+++ UBDCalculator/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var inputField: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: UIButton) {
+        let input = Float(inputField.text!)
+        var output:Float
+        
+        guard let input_ = input else {
+            outputLabel.text = "값을 입력하십시오."
+            return
+        }
+        output = input_/172212.0
+        outputLabel.text = String(output) + " UBD"
+    }
+    
 }
 
